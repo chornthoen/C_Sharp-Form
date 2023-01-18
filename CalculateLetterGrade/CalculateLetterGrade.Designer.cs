b88c0074c5a01097ba158080276152a1f9a61cee
thoen <thoenchorn@gmail.com>
2023-01-19 00:47:33 +0700

    first commit

---
+++ CalculateLetterGrade/CalculateLetterGrade.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace CalculateLetterGrade
+{
+    partial class CalculateLetterGrade
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNumericGrade = new System.Windows.Forms.TextBox();
+            this.txtLetterGrade = new System.Windows.Forms.TextBox();
+            this.btnCalculateLetterGrade = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(99, 74);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Numeric Grade";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(99, 112);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Letter Grade";
+            // 
+            // txtNumericGrade
+            // 
+            this.txtNumericGrade.Location = new System.Drawing.Point(230, 69);
+            this.txtNumericGrade.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtNumericGrade.Name = "txtNumericGrade";
+            this.txtNumericGrade.Size = new System.Drawing.Size(133, 20);
+            this.txtNumericGrade.TabIndex = 1;
+            // 
+            // txtLetterGrade
+            // 
+            this.txtLetterGrade.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.txtLetterGrade.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.txtLetterGrade.Location = new System.Drawing.Point(230, 110);
+            this.txtLetterGrade.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtLetterGrade.Name = "txtLetterGrade";
+            this.txtLetterGrade.ReadOnly = true;
+            this.txtLetterGrade.Size = new System.Drawing.Size(133, 20);
+            this.txtLetterGrade.TabIndex = 3;
+            this.txtLetterGrade.TabStop = false;
+            this.txtLetterGrade.TextChanged += new System.EventHandler(this.txtLetterGrade_TextChanged);
+            // 
+            // btnCalculateLetterGrade
+            // 
+            this.btnCalculateLetterGrade.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCalculateLetterGrade.Location = new System.Drawing.Point(158, 170);
+            this.btnCalculateLetterGrade.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnCalculateLetterGrade.Name = "btnCalculateLetterGrade";
+            this.btnCalculateLetterGrade.Size = new System.Drawing.Size(60, 53);
+            this.btnCalculateLetterGrade.TabIndex = 2;
+            this.btnCalculateLetterGrade.Text = "&Calculate Letter Grade";
+            this.btnCalculateLetterGrade.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.AutoSize = true;
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(306, 170);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(56, 53);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // CalculateLetterGrade
+            // 
+            this.AcceptButton = this.btnCalculateLetterGrade;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculateLetterGrade);
+            this.Controls.Add(this.txtLetterGrade);
+            this.Controls.Add(this.txtNumericGrade);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "CalculateLetterGrade";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculate Letter Grade";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNumericGrade;
+        private System.Windows.Forms.TextBox txtLetterGrade;
+        private System.Windows.Forms.Button btnCalculateLetterGrade;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ CalculateLetterGrade/CalculateLetterGrade.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculateLetterGrade
+{
+    public partial class CalculateLetterGrade : Form
+    {
+        public CalculateLetterGrade()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtLetterGrade_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
